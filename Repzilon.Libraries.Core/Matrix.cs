885af5b974d695bfbf5ed50bb57750f474fb59ab
repzilon <241029-repzilon@users.noreply.gitlab.com>
2022-11-18 02:09:01 -0500

    Basic implementation of common interfaces, addition, subtraction, scalar product and matrix product, with a test program.

--- Repzilon.Libraries.Core/Matrix.cs
+++ Repzilon.Libraries.Core/Matrix.cs
@@ -1,9 +1,308 @@
-﻿using System;
+﻿//
+//  Matrix.cs
+//
+//  Author:
+//       René Rhéaume <repzilon@users.noreply.github.com>
+//
+// Copyright (C) 2022 René Rhéaume
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq.Expressions;
+using System.Text;
 
 namespace Repzilon.Libraries.Core
 {
-    public class Class1
-    {
-    }
-}
+	public struct Matrix<T> : ICloneable, IEquatable<Matrix<T>> where T : struct, IConvertible, IEquatable<T>
+	{
+		public readonly byte Lines;
+		public readonly byte Columns;
+		private readonly T[,] m_values;
 
+		#region Constructors
+		public Matrix(byte lines, byte columns)
+		{
+			Lines = lines;
+			Columns = columns;
+			m_values = new T[lines, columns];
+		}
+
+		public Matrix(byte lines, byte columns, params T[] lineByLineValues) : this(lines, columns)
+		{
+			if ((lineByLineValues != null) && (lineByLineValues.Length > 0)) {
+				var count = lines * columns;
+				if (lineByLineValues.Length != count) {
+					throw new ArgumentException(String.Format(CultureInfo.CurrentCulture,
+					 "For this {0}x{1} matrix, {2} values are expected.",
+					 lines, columns, count));
+				}
+				for (int i = 0; i < count; i++) {
+					m_values[i / columns, i % columns] = lineByLineValues[i];
+				}
+			}
+		}
+
+		public static Matrix<T> Identity(byte size)
+		{
+			var m = new Matrix<T>(size, size);
+			for (byte i = 0; i < size; i++) {
+				m[i, i] = (T)Convert.ChangeType(1, typeof(T));
+			}
+			return m;
+		}
+		#endregion
+
+		public T this[byte l, byte c] {
+			get { return m_values[l, c]; }
+			set { m_values[l, c] = value; }
+		}
+
+		#region ICloneable members
+		public Matrix<T> Clone()
+		{
+			var other = new Matrix<T>(this.Lines, this.Columns);
+			for (byte i = 0; i < this.Lines; i++) {
+				for (byte j = 0; j < this.Columns; j++) {
+					other[i, j] = this[i, j];
+				}
+			}
+			return other;
+		}
+
+		object ICloneable.Clone()
+		{
+			return this.Clone();
+		}
+		#endregion
+
+		public static Matrix<TOut> Cast<TIn, TOut>(Matrix<TIn> source)
+		where TIn : struct, IConvertible, IEquatable<TIn>
+		where TOut : struct, IConvertible, IEquatable<TOut>
+		{
+			var other = new Matrix<TOut>(source.Lines, source.Columns);
+			for (byte i = 0; i < source.Lines; i++) {
+				for (byte j = 0; j < source.Columns; j++) {
+					other[i, j] = (TOut)Convert.ChangeType(source[i, j], typeof(TOut));
+				}
+			}
+			return other;
+		}
+
+		public Matrix<TOut> Cast<TOut>() where TOut : struct, IConvertible, IEquatable<TOut>
+		{
+			return Cast<T, TOut>(this);
+		}
+
+		#region Equals
+		public bool Equals(Matrix<T> other)
+		{
+			if (this.SameSize(other)) {
+				for (byte i = 0; i < this.Lines; i++) {
+					for (byte j = 0; j < this.Columns; j++) {
+						if (!other[i, j].Equals(this[i, j])) {
+							return false;
+						}
+					}
+				}
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public override bool Equals(object obj)
+		{
+			return (obj != null) && (obj is Matrix<T>) && this.Equals((Matrix<T>)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			int hashCode = 1317424563;
+			hashCode = hashCode * -1521134295 + Lines.GetHashCode();
+			hashCode = hashCode * -1521134295 + Columns.GetHashCode();
+			hashCode = hashCode * -1521134295 + EqualityComparer<T[,]>.Default.GetHashCode(m_values);
+			return hashCode;
+		}
+
+		public bool SameSize(Matrix<T> other)
+		{
+			return (other.Lines == this.Lines) && (other.Columns == this.Columns);
+		}
+		#endregion
+
+		public override string ToString()
+		{
+			StringBuilder stbDesc = new StringBuilder();
+			for (byte i = 0; i < this.Lines; i++) {
+				if (this.Lines == 1) {
+					stbDesc.Append('[');
+				} else if (i == 0) {
+					stbDesc.Append('/');
+				} else if (i == this.Lines - 1) {
+					stbDesc.Append('\\');
+				} else {
+					stbDesc.Append('|');
+				}
+
+				for (byte j = 0; j < this.Columns; j++) {
+					if (j > 0) {
+						stbDesc.Append('\t');
+					}
+					stbDesc.Append(this[i, j]);
+				}
+
+				if (this.Lines == 1) {
+					stbDesc.Append(']');
+				} else if (i == 0) {
+					stbDesc.Append('\\');
+				} else if (i == this.Lines - 1) {
+					stbDesc.Append('/');
+				} else {
+					stbDesc.Append('|');
+				}
+
+				if (i < this.Lines - 1) {
+					stbDesc.Append(Environment.NewLine);
+				}
+			}
+			stbDesc.AppendFormat(" {0}x{1} of {2}", this.Lines, this.Columns, typeof(T));
+			return stbDesc.ToString();
+		}
+
+		#region Operators
+		private static Func<T, T, T> BuildAdder()
+		{
+			// Declare the parameters
+			var paramA = Expression.Parameter(typeof(T), "a");
+			var paramB = Expression.Parameter(typeof(T), "b");
+
+			// Add the parameters together
+			BinaryExpression body = Expression.Add(paramA, paramB);
+
+			// Compile it
+			return Expression.Lambda<Func<T, T, T>>(body, paramA, paramB).Compile();
+		}
+
+		public static Matrix<T> operator +(Matrix<T> a, Matrix<T> b)
+		{
+			if (b.SameSize(a)) {
+				var add = BuildAdder();
+				var m = new Matrix<T>(a.Lines, a.Columns);
+				for (byte i = 0; i < a.Lines; i++) {
+					for (byte j = 0; j < a.Columns; j++) {
+						m[i, j] = add(a[i, j], b[i, j]);
+					}
+				}
+				return m;
+			} else {
+				throw new ArrayTypeMismatchException(String.Format(CultureInfo.CurrentCulture,
+				 "To add matrices, their dimensions must be identical. They are {0}x{1} and {2}x{3}.",
+				 a.Lines, a.Columns, b.Lines, b.Columns));
+			}
+		}
+
+		private static Func<T, T, T> BuildSubtractor()
+		{
+			// Declare the parameters
+			var paramA = Expression.Parameter(typeof(T), "a");
+			var paramB = Expression.Parameter(typeof(T), "b");
+
+			// Add the parameters together
+			BinaryExpression body = Expression.Subtract(paramA, paramB);
+
+			// Compile it
+			return Expression.Lambda<Func<T, T, T>>(body, paramA, paramB).Compile();
+		}
+
+		public static Matrix<T> operator -(Matrix<T> a, Matrix<T> b)
+		{
+			if (b.SameSize(a)) {
+				var sub = BuildSubtractor();
+				var m = new Matrix<T>(a.Lines, a.Columns);
+				for (byte i = 0; i < a.Lines; i++) {
+					for (byte j = 0; j < a.Columns; j++) {
+						m[i, j] = sub(a[i, j], b[i, j]);
+					}
+				}
+				return m;
+			} else {
+				throw new ArrayTypeMismatchException(String.Format(CultureInfo.CurrentCulture,
+				 "To subtract matrices, their dimensions must be identical.",
+				 a.Lines, a.Columns, b.Lines, b.Columns));
+			}
+		}
+
+		private static Func<TScalar, T, T> BuildMultiplier<TScalar>() where TScalar : struct, IConvertible, IEquatable<T>
+		{
+			// Declare the parameters
+			var paramA = Expression.Parameter(typeof(TScalar), "a");
+			var paramB = Expression.Parameter(typeof(T), "b");
+
+			// Add the parameters together
+			BinaryExpression body = Expression.Multiply(paramA, paramB);
+
+			// Compile it
+			return Expression.Lambda<Func<TScalar, T, T>>(body, paramA, paramB).Compile();
+		}
+
+		public static Matrix<T> Multiply<TScalar>(TScalar k, Matrix<T> m) where TScalar : struct, IConvertible, IEquatable<T>
+		{
+			var mul = BuildMultiplier<TScalar>();
+			var mm = new Matrix<T>(m.Lines, m.Columns);
+			for (byte i = 0; i < m.Lines; i++) {
+				for (byte j = 0; j < m.Columns; j++) {
+					mm[i, j] = mul(k, m[i, j]);
+				}
+			}
+			return mm;
+		}
+
+		public static Matrix<T> operator *(T k, Matrix<T> m)
+		{
+			return Multiply(k, m);
+		}
+
+		public Matrix<T> Multiply<TScalar>(TScalar k) where TScalar : struct, IConvertible, IEquatable<T>
+		{
+			return Multiply(k, this);
+		}
+
+		public static Matrix<T> operator *(Matrix<T> a, Matrix<T> b)
+		{
+			if (a.Columns == b.Lines) {
+				var add = BuildAdder();
+				var mul = BuildMultiplier<T>();
+				var c = new Matrix<T>(a.Lines, b.Columns);
+				for (byte i = 0; i < c.Lines; i++) {
+					for (byte j = 0; j < c.Columns; j++) {
+						T sumOfCell = default(T);
+						for (byte x = 0; x < b.Lines; x++) {
+							sumOfCell = add(sumOfCell, mul(a[i,x], b[x,j]));
+						}
+						c[i, j] = sumOfCell;
+					}
+				}
+				return c;
+			} else {
+				throw new ArrayTypeMismatchException(String.Format(CultureInfo.CurrentCulture,
+				 "Cannot multiply a {0}x{1} matrix with a {2}x{3} matrix.",
+				 a.Lines, a.Columns, b.Lines, b.Columns));
+			}
+		}
+		#endregion
+	}
+}
---
+++ Repzilon.Tests.Matrix/Program.cs
@@ -0,0 +1,66 @@
+﻿//
+//  Program.cs
+//
+//  Author:
+//       René Rhéaume <repzilon@users.noreply.github.com>
+//
+// Copyright (C) 2022 René Rhéaume
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+using System;
+using Repzilon.Libraries.Core;
+
+namespace Repzilon.Tests.Matrix
+{
+	public static class Program
+	{
+		static void Main()
+		{
+			var ex80_a = new Matrix<short>(3, 3, 2, -1, 5, -3, 4, 7, 1, -1, 0);
+			var ex80_b = new Matrix<short>(3, 3, 1, 4, 0, -3, 6, -5, 1, 0, -1);
+			var ex80_result = (3 * ex80_a) + ex80_b;
+			Console.WriteLine(ex80_a);
+			Console.WriteLine(ex80_b);
+			Console.WriteLine(ex80_result);
+
+			var ex81_a = new Matrix<short>(2, 2, 1, 3, -2, 1);
+			var ex81_b = new Matrix<short>(2, 3, 1, 2, 1, -3, 4, -5);
+			try {
+				var ex81_ab = ex81_a * ex81_b;
+				Console.WriteLine(ex81_ab);
+				var ex81_ba = ex81_b * ex81_a;
+				Console.WriteLine(ex81_ba);
+			} catch (Exception ex) {
+				Console.Error.WriteLine(ex.ToString());
+			}
+
+			var pap_m = new Matrix<short>(3, 3, 4, 3, 5, -3, -7, 1, 8, 0, 0);
+			var pap_n = new Matrix<short>(3, 3, 4, 5, 1, 0, 1, -4, 6, 2, -1);
+			var pap_a = pap_m + pap_n;
+			var pap_b = (2 * pap_n) - (5 * pap_m);
+			var pap_c = pap_m * pap_n;
+			var pap_d = pap_n * pap_m;
+
+			Console.WriteLine(pap_a);
+			Console.WriteLine(pap_b);
+			Console.WriteLine(pap_c);
+			Console.WriteLine(pap_d);
+
+			Console.WriteLine("Press Enter to exit...");
+			Console.ReadLine();
+		}
+	}
+}
+
