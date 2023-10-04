35f7df80426c4a6c59f4a2629e509000fedfd438
repzilon <6383002+repzilon@users.noreply.github.com>
2023-10-04 11:25:49 -0400

    First version of SignificantDigits that works reasonably well over several numeric types

---
+++ Repzilon.Libraries.Core/SignificantDigits.cs
@@ -0,0 +1,223 @@
+﻿//
+//  SignificantDigits.cs
+//
+//  Author:
+//       René Rhéaume <repzilon@users.noreply.github.com>
+//
+// Copyright (C) 2022-2023 René Rhéaume
+//
+// This Source Code Form is subject to the terms of the 
+// Mozilla Public License, v. 2.0. If a copy of the MPL was 
+// not distributed with this file, You can obtain one at 
+// https://mozilla.org/MPL/2.0/.
+//
+using System;
+using System.Globalization;
+
+namespace Repzilon.Libraries.Core
+{
+	public enum RoundingMode : byte
+	{
+		Floor,
+		ToEven,
+		AwayFromZero,
+		Ceiling
+	}
+
+	public static class SignificantDigits
+	{
+		public static byte Count(int value)
+		{
+			if (value == 0) {
+				return 1;
+			}
+			while ((value % 10) == 0) {
+				value /= 10;
+			}
+			return (value >= -1) && (value <= 1) ? (byte)1 : (byte)Math.Ceiling(Math.Log10(Math.Abs(value)));
+		}
+
+		public static byte Count(long value)
+		{
+			if (value == 0) {
+				return 1;
+			}
+			while ((value % 10) == 0) {
+				value /= 10;
+			}
+			return (value >= -1) && (value <= 1) ? (byte)1 : (byte)Math.Ceiling(Math.Log10(Math.Abs(value)));
+		}
+
+		public static byte Count(float value)
+		{
+			if (value == 0) {
+				return 1;
+			}
+			var sngAbsolute = Math.Abs(value);
+			var sngDigitalPart = Round.Error((float)(sngAbsolute - Math.Floor(sngAbsolute)));
+			while ((value % 10) == 0) {
+				value /= 10;
+			}
+			byte bytDigits;
+			if (sngAbsolute < 1) {
+				bytDigits = (sngDigitalPart != 0) ? (byte)0 : (byte)1;
+			} else if (sngAbsolute == 1) {
+				bytDigits = 1;
+			} else {
+				// Do not replace Math.Abs(value) with sngAbsolute, because we changed value with the loop above
+				bytDigits = (byte)Math.Ceiling(Math.Log10(Math.Abs(value)));
+			}
+			if (sngDigitalPart != 0) {
+				// Microsoft recommends G9 instead of R for Single, but G9 causes trouble in fact.
+				var strForCount = sngDigitalPart.ToString("R", CultureInfo.InvariantCulture).Replace("0.", "");
+				if (sngAbsolute < 1) {
+					strForCount = strForCount.TrimStart('0');
+				}
+				bytDigits += (byte)strForCount.Length;
+			}
+			return bytDigits;
+		}
+
+		public static byte Count(double value)
+		{
+			if (value == 0) {
+				return 1;
+			}
+			var dblAbsolute = Math.Abs(value);
+			var dblDigitalPart = Round.Error(dblAbsolute - Math.Floor(dblAbsolute));
+			while ((value % 10) == 0) {
+				value /= 10;
+			}
+			byte bytDigits;
+			if (dblAbsolute < 1) {
+				bytDigits = (dblDigitalPart != 0) ? (byte)0 : (byte)1;
+			} else if (dblAbsolute == 1) {
+				bytDigits = 1;
+			} else {
+				// Do not replace Math.Abs(value) with sngAbsolute, because we changed value with the loop above
+				bytDigits = (byte)Math.Ceiling(Math.Log10(Math.Abs(value)));
+			}
+			if (dblDigitalPart != 0) {
+				// Microsoft recommends G17 instead of R for Double, but G17 causes trouble in fact.
+				var strForCount = dblDigitalPart.ToString("R", CultureInfo.InvariantCulture).Replace("0.", "");
+				if (dblAbsolute < 1) {
+					strForCount = strForCount.TrimStart('0');
+				}
+				bytDigits += (byte)strForCount.Length;
+			}
+			return bytDigits;
+		}
+
+		public static byte Count(decimal value)
+		{
+			if (value == 0) {
+				return 1;
+			}
+			var dcmAbsolute = Math.Abs(value);
+			var dcmDigitalPart = dcmAbsolute - Math.Floor(dcmAbsolute);
+			while ((value % 10) == 0) {
+				value /= 10;
+			}
+			byte bytDigits;
+			if (dcmAbsolute < 1) {
+				bytDigits = (dcmDigitalPart != 0) ? (byte)0 : (byte)1;
+			} else if (dcmAbsolute == 1) {
+				bytDigits = 1;
+			} else {
+				// Do not replace Math.Abs(value) with sngAbsolute, because we changed value with the loop above
+				bytDigits = (byte)Math.Ceiling(Math.Log10(Math.Abs((double)value)));
+			}
+			if (dcmDigitalPart != 0) {
+				// Format with maximum decimals, then remove trailing zeros
+				var strForCount = dcmDigitalPart.ToString("f17", CultureInfo.InvariantCulture).Replace("0.", "").TrimEnd('0');
+				if (dcmAbsolute < 1) {
+					strForCount = strForCount.TrimStart('0');
+				}
+				bytDigits += (byte)strForCount.Length;
+			}
+			return bytDigits;
+		}
+
+#if !NETSTANDARD1_1
+		public static byte Count(IConvertible value)
+		{
+			if (value == null) {
+				throw new ArgumentNullException("value");
+			}
+			var enuTC = value.GetTypeCode();
+			if (enuTC == TypeCode.String) {
+				throw new NotImplementedException("String support is planned.");
+			} else if (enuTC == TypeCode.Boolean || enuTC == TypeCode.Char || enuTC == TypeCode.DateTime || enuTC == TypeCode.Empty || enuTC == TypeCode.Object) {
+				throw new ArgumentException("The argument is neither a number nor a string.", "value");
+			} else if (enuTC == TypeCode.Int32) {
+				return Count((int)value);
+			} else if (enuTC == TypeCode.Byte || enuTC == TypeCode.Int16 || enuTC == TypeCode.SByte || enuTC == TypeCode.UInt16) {
+				return Count(Convert.ToInt32(value));
+			} else if (enuTC == TypeCode.Int64) {
+				return Count((long)value);
+			} else if (enuTC == TypeCode.UInt32) {
+				return Count(Convert.ToInt64(value));
+			} else if (enuTC == TypeCode.UInt64) {
+				return ((ulong)value > Int64.MaxValue) ? Count(Convert.ToDecimal(value)) : Count((long)value);
+			} else if (enuTC == TypeCode.Double) {
+				return Count((double)value);
+			} else if (enuTC == TypeCode.Single) {
+				return Count((float)value);
+			} else if (enuTC == TypeCode.Decimal) {
+				return Count((decimal)value);
+			} else {
+				throw new NotSupportedException("The argument is of an unknown type.");
+			}
+		}
+#endif
+
+#if (false)
+		public static byte Count(string value)
+		{
+
+		}
+
+		public static byte Count(string value, CultureInfo culture)
+		{
+
+		}
+
+		public static decimal Round(decimal value, byte figures, RoundingMode rounding)
+		{
+			
+		}
+
+		public static double Round(double value, byte figures, RoundingMode rounding)
+		{
+
+		}
+
+		public static float Round(float value, byte figures, RoundingMode rounding)
+		{
+
+		}
+#endif
+
+		private static double ParseQty(string value)
+		{
+			const NumberStyles kNumberStyles = NumberStyles.Number | NumberStyles.AllowExponent | NumberStyles.AllowCurrencySymbol;
+			double dblValue;
+
+			if (!Double.TryParse(value, kNumberStyles, CultureInfo.CurrentCulture, out dblValue)) {
+				if (!Double.TryParse(value, kNumberStyles, CultureInfo.CurrentUICulture, out dblValue)) {
+#if NETSTANDARD2_0
+					if (!Double.TryParse(value, kNumberStyles, CultureInfo.InstalledUICulture, out dblValue)) {
+#endif
+						if (!Double.TryParse(value, kNumberStyles, CultureInfo.InvariantCulture, out dblValue)) {
+							throw new FormatException("Unable to parse text as a number.");
+						}
+#if NETSTANDARD2_0
+					}
+#endif
+				}
+			}
+			return dblValue;
+		}
+	}
+}
+
---
+++ Repzilon.Tests.Digits/Program.cs
@@ -0,0 +1,79 @@
+﻿//
+//  Program.cs
+//
+//  Author:
+//       René Rhéaume <repzilon@users.noreply.github.com>
+//
+// Copyright (C) 2023 René Rhéaume
+//
+// This Source Code Form is subject to the terms of the 
+// Mozilla Public License, v. 2.0. If a copy of the MPL was 
+// not distributed with this file, You can obtain one at 
+// https://mozilla.org/MPL/2.0/.
+//
+using Repzilon.Libraries.Core;
+using System;
+
+namespace Repzilon.Tests.Digits
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Action<string> toConsole = Console.WriteLine;
+			try {
+				TestDigitCount(new short[] { -2, -1, 0, 1, 2, 123, 2005, 2000 }, new byte[] { 1, 1, 1, 1, 1, 3, 4, 1 }, toConsole);
+				Console.Write(Environment.NewLine);
+				TestDigitCount(new int[] { -2, -1, 0, 1, 2, 123, 2005, 2000 }, new byte[] { 1, 1, 1, 1, 1, 3, 4, 1 }, toConsole);
+				Console.Write(Environment.NewLine);
+				TestDigitCount(new long[] { -2, -1, 0, 1, 2, 123, 2005, 2000 }, new byte[] { 1, 1, 1, 1, 1, 3, 4, 1 }, toConsole);
+				Console.Write(Environment.NewLine);
+				TestDigitCount(new ushort[] { 0, 1, 2, 123, 2005, 2000 }, new byte[] { 1, 1, 1, 3, 4, 1 }, toConsole);
+				Console.Write(Environment.NewLine);
+				TestDigitCount(
+				 new float[] { -2, -1, 0, 1, 2, 123f, 43.567f, 0.0054f, 0.124f, 25.02f, 2005f, 3.00f, 300.0f, 2000f },
+				 new byte[] { 1, 1, 1, 1, 1, 3, 5, 2, 3, 4, 4, 1, 1, 1 }, toConsole);
+				Console.Write(Environment.NewLine);
+				TestDigitCount(
+				 new double[] { -2, -1, 0, 1, 2, 123, 43.567, 0.0054, 0.124, 25.02, 2005, 3.00, 300.0, 2000 },
+				 new byte[] { 1, 1, 1, 1, 1, 3, 5, 2, 3, 4, 4, 1, 1, 1 }, toConsole);
+				Console.Write(Environment.NewLine);
+				TestDigitCount(
+				 new decimal[] { -2, -1, 0, 1, 2, 123, 43.567m, 0.0054m, 0.124m, 25.02m, 2005, 3.00m, 300.0m, 2000 },
+				 new byte[] { 1, 1, 1, 1, 1, 3, 5, 2, 3, 4, 4, 1, 1, 1 }, toConsole);
+			} catch (Exception ex) {
+				Console.Error.WriteLine(ex);
+			}
+
+			Console.WriteLine("Press Enter to exit...");
+			Console.ReadLine();
+		}
+
+		private static bool[] TestDigitCount<T>(
+		T[] values, byte[] expectedCounts, Action<string> messageWriter) where T : IConvertible
+		{
+			if ((values != null) && (expectedCounts != null)) {
+				int c = values.Length;
+				if (expectedCounts.Length != c) {
+					throw new ArgumentException("The count of elements in each of the passed arrays must be identical.");
+				}
+				if (messageWriter != null) {
+					messageWriter(String.Format("Data type: {0}", typeof(T).Name));
+				}
+
+				bool[] blnarOK = new bool[c];
+				for (int i = 0; i < c; i++) {
+					var d = SignificantDigits.Count(values[i]);
+					blnarOK[i] = d == expectedCounts[i];
+					if (messageWriter != null) {
+						messageWriter(String.Format(
+						 blnarOK[i] ? "{0}\t-> {1} correct" : "{0}\t-> {1} WRONG (should be {2})",
+						 values[i], d, expectedCounts[i]));
+					}
+				}
+				return blnarOK;
+			}
+			return Array.Empty<bool>();
+		}
+	}
+}
