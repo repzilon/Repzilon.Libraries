c7e1f4a7b1b4cb4d3d67f006a0b994aad1566f65
repzilon <6383002+repzilon@users.noreply.github.com>
2023-10-12 15:35:55 -0400

    Moving all tests to a single program

---
+++ Repzilon.Tests.ForCoreLibrary/Program.cs
@@ -0,0 +1,28 @@
+﻿//
+//  Program.cs
+//
+//  Author:
+//       René Rhéaume <repzilon@users.noreply.github.com>
+//
+// Copyright (C) 2023 René Rhéaume
+//
+// This Source Code Form is subject to the terms of the 
+// Mozilla Public License, v. 2.0. If a copy of the MPL was 
+// not distributed with this file, You can obtain one at 
+// https://mozilla.org/MPL/2.0/.
+//
+using System;
+
+namespace Repzilon.Tests.ForCoreLibrary
+{
+	static class Program
+	{
+
+
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
+
