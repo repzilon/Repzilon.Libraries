6ebae3ed89606b51145042a17450166fa596880b
René Rhéaume <repzilon@users.noreply.github.com>
2024-01-21 15:13:14 -0500

    Removed trailing whitespace

--- Repzilon.Libraries.Core/Interfaces/ICartesianVector.cs
+++ Repzilon.Libraries.Core/Interfaces/ICartesianVector.cs
@@ -6,9 +6,9 @@
 //
 // Copyright (C) 2023-2024 René Rhéaume
 //
-// This Source Code Form is subject to the terms of the 
-// Mozilla Public License, v. 2.0. If a copy of the MPL was 
-// not distributed with this file, You can obtain one at 
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL was
+// not distributed with this file, You can obtain one at
 // https://mozilla.org/MPL/2.0/.
 //
 using System;
