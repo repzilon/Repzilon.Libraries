6deedd7479de4c5b41479fedc9eb5c924fc5200e
repzilon <6383002+repzilon@users.noreply.github.com>
2025-01-07 19:02:23 -0500

    Added license headers
    The Javascript code was ported from C# and bears the copyright year span of the C# code

--- AminoLateralCharge_full.js
+++ AminoLateralCharge_full.js
@@ -1,4 +1,19 @@
-exports = {};
+//
+//  AminoLateralCharge_full.js
+//
+//  Author:
+//       René Rhéaume <repzilon@users.noreply.github.com>
+//
+// Copyright (C) 2022-2025 René Rhéaume
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL was
+// not distributed with this file, You can obtain one at
+// https://mozilla.org/MPL/2.0/.
+//
+if (!window.exports) {
+	exports = {};
+}
 exports.RoundOff = (function () {
 	function RoundOff() { }
 	RoundOff.error = function (value) {
