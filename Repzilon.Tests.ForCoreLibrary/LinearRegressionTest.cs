ee9e6c0155263c310d11403bb1f8c9ec404c3070
repzilon <6383002+repzilon@users.noreply.github.com>
2025-05-04 07:20:02 -0400

    Rider incorrectly flagged this using directive as useless

--- Repzilon.Tests.ForCoreLibrary/LinearRegressionTest.cs
+++ Repzilon.Tests.ForCoreLibrary/LinearRegressionTest.cs
@@ -14,6 +14,9 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+#if !NET20
+using System.Linq;
+#endif
 using Repzilon.Libraries.Core;
 using Repzilon.Libraries.Core.Regression;
 // ReSharper disable InconsistentNaming
